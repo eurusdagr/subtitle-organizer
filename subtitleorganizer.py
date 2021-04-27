77cd671fcf977674518af54fb928a5e5ee121cb3
eurusdagr <82729967+eurusdagr@users.noreply.github.com>
2021-04-27 17:14:29 -0300

    Update subtitleorganizer.py
    fixed formatting and added some error handling

--- subtitleorganizer.py
+++ subtitleorganizer.py
@@ -5,31 +5,29 @@ import shutil
 
 ia = imdb.IMDb()
 directory = os.chdir(input())
-
 for file in os.listdir(directory):
-  movie = ia.search_movie(file)
-  movie = (movie[0].getID())
-  movie = ia.get_movie(movie)
-  print(movie)
-  try:
-    for director in movie['directors']:
-     print(director)
-    directorname = director['name']
-    if os.path.isdir(directorname) == True:
-        shutil.move(file,directorname)
+    try:
+        movie = ia.search_movie(file)
+        movie = movie[0].getID()
+        movie = ia.get_movie(movie)
+        print(movie)
+    except IndexError:
+        print("API didn't find anything")
     else:
-        os.mkdir(director['name'])
-        shutil.move(file, directorname)
-  except KeyError:
-    print('no information')
+        try:
+            for director in movie["directors"]:
 
-
-
-
-
-
-
-
-
-
-
+                try:
+                    print(director)
+                    directorname = director["name"]
+                    if os.path.isdir(directorname) == True:
+                        shutil.move(file, directorname)
+                    else:
+                        os.mkdir(director["name"])
+                        shutil.move(file, directorname)
+                except TypeError:
+                    print("")
+                    pass
+        except KeyError:
+            print("no information")
+            pass
