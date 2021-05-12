0d8e31459196837f6443e96c7cd24b80dd41f3cc
eurusdagr <82729967+eurusdagr@users.noreply.github.com>
2021-05-12 15:52:18 -0300

    Update subtitleorganizer.py
    add some error handling and made ignores if files is a directory so if you have to rerun it you wouldn't get director names thrown into new folders

--- subtitleorganizer.py
+++ subtitleorganizer.py
@@ -2,32 +2,45 @@ import imdb
 import os
 import shutil
 
-
 ia = imdb.IMDb()
 directory = os.chdir(input())
 for file in os.listdir(directory):
-    try:
-        movie = ia.search_movie(file)
-        movie = movie[0].getID()
-        movie = ia.get_movie(movie)
-        print(movie)
-    except IndexError:
-        print("API didn't find anything")
+    if os.path.isdir(file) == True:
+        pass
+
     else:
         try:
-            for director in movie["directors"]:
+            movie = ia.search_movie(file)
+            selected_movie = movie[0]
+            for m in movie:
+                if "Japanese" in m.get("languages", []):  # or use another key
+                    movie = m
+                    break
+            movie = selected_movie.getID()
+            movie = ia.get_movie(movie)
+        except IndexError:
+            print("API didn't find anything")
+        else:
+            try:
+                for director in movie["directors"]:
 
-                try:
-                    print(director)
-                    directorname = director["name"]
-                    if os.path.isdir(directorname) == True:
-                        shutil.move(file, directorname)
-                    else:
-                        os.mkdir(director["name"])
-                        shutil.move(file, directorname)
-                except TypeError:
-                    print("")
-                    pass
-        except KeyError:
-            print("no information")
-            pass
+                    try:
+                        print(director)
+                        directorname = director["name"]
+                        if os.path.isdir(directorname) == True:
+                            shutil.move(file, directorname)
+                        else:
+                            os.mkdir(director["name"])
+                            shutil.move(file, directorname)
+                    except TypeError:
+                        print("")
+                        pass
+                    except shutil.Error:
+
+                        pass
+                    except FileNotFoundError:
+                        pass
+
+            except KeyError:
+                print("no information")
+                pass
