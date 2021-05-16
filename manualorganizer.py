166e3ab6fb8ab3270751cbaad29dd6b3f6869f0c
eurusdagr <82729967+eurusdagr@users.noreply.github.com>
2021-05-15 23:58:36 -0300

    Add files via upload

---
+++ manualorganizer.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import re
+import pyperclip
+
+
+directory = os.chdir(input())
+for file in os.listdir(directory):
+    if os.path.isdir(file) == True:
+        pass
+    else:
+        try:
+         file1 = os.path.splitext(file)[0]
+         pyperclip.copy(file1)
+         print('name of the movie was copied to your clipboard')
+         print('Insert the name of the director of the following movie: ',file1)
+         foldername = input()
+         if os.path.isdir(foldername) == True:
+            shutil.move(file, foldername)
+            print("copied file to the",foldername,"folder")
+         else:
+            os.mkdir(foldername)
+            shutil.move(file, foldername)
+            print("created",foldername,"folder and copied the file")
+
+        except shutil.Error:
+         pass
+        except FileNotFoundError:
+         pass
+
